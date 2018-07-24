b4f2abf8213046cb279e73e69a13205c35ec1299
Manuel <manuel@2dmedia.mx>
2018-07-24 18:21:19 -0500

    Actividad 5

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -1,7 +1,13 @@
 package unam.fca.dmoviles;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,31 +16,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Actividad 1 Introduccion a TextView.
-        //https://developer.android.com/reference/android/widget/TextView
-        //TextView es un componente de Android que permite desplegar texto.
+        //Actividad 5 Recibir datos de otras aplicaciones
+        //https://developer.android.com/training/sharing/receive
+        //Las aplicaciones en android pueden recibir informacion de otras apps
 
         // El archivo del layout de la actividad se encuentra en la carpeta
         // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
 
-        //Para hacer referencia a un componente del layout, utilizamos el metodo findViewById
-        //y le pasamos como parametro el id correspondiente.
-        //Para hacer referencia al Textview con id tv
-        TextView tv = findViewById(R.id.tv);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        final EditText et = findViewById(R.id.et);
 
 
-        //Para cambiar el texto de tv
-        tv.setText("Hola mundo");
+        //Cuando nuestra aplicacion sea iniciada por otra  aplicacion, type no sera nulo
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+
+            String extraText = intent.getStringExtra(Intent.EXTRA_TEXT);
+            if (extraText != null) {
+                et.setText(extraText);
+            }
+
+        }
+
 
         //Actividades
-        //a) Modifica el texto "Hola mundo", pon tu nombre y ejecuta la aplicacion
-        //b) Crea una entrada en la carpeta res/values/strings.xml con tu numero de cuenta
-        //c) modifica setText para que lea esta entrada de texto (Tip, puedes hacer referencia al texto con R.string.<id entrada>)
-        //d) Ejecuta la aplicacion
-        //e) Sube tu codigo al repositorio.
-        //f) Sube un documento en word a la plataforma Moodle con las capturas de pantalla de tu actividad. Incluye la liga a tu repositorio
+        //a) Descomenta las lineas del archivo AndroidManifest.xml
+        //b) Revisa la url de referencia, que tipo de informacion puede recibir la app?
+        //c) Ejecuta la aplicacion en modo de depuracion, la primera vez que se ejecuta la app, que valor tiene Intent.ACTION_SEND?
+        //d) Prueba compartiendo un video de YouTube o una ubicacion de GoogleMaps a la aplicacion
+        //e) Cuando la aplicacion es iniciada por otra app, que valor tiene Intent.ACTION_SEND?
+        //f) Anota tus respuestas en un documento en Word
+        //g) Sube tu codigo al repositorio.
+        //h) Sube el documento Word con tus respuestas y las capturas de pantalla  a la plataforma Moodle. Incluye la liga a tu repositorio
+
 
     }
 }
