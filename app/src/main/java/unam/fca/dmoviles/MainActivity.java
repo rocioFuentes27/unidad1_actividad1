abccb9ad84f34c4158f994b9522a7432dd7e141b
Manuel <manuel@2dmedia.mx>
2018-07-24 10:24:44 -0500

    Actividad 2

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,6 +2,7 @@ package unam.fca.dmoviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Actividad 1 Introduccion a TextView.
-        //https://developer.android.com/reference/android/widget/TextView
-        //TextView es un componente de Android que permite desplegar texto.
+        //Actividad 2 Introduccion a eventos con Button
+        //https://developer.android.com/reference/android/widget/Button
+        //Button es un componente de Android que realizar acciones al hacer click en el
 
         // El archivo del layout de la actividad se encuentra en la carpeta
         // res/layout/activity_main.xml
@@ -21,18 +22,17 @@ public class MainActivity extends AppCompatActivity {
 
         //Para hacer referencia a un componente del layout, utilizamos el metodo findViewById
         //y le pasamos como parametro el id correspondiente.
-        //Para hacer referencia al Textview con id tv
-        TextView tv = findViewById(R.id.tv);
+        TextView tv = findViewById(R.id.btn);
+
+        Button btn = findViewById(R.id.btn);
 
 
         //Para cambiar el texto de tv
         tv.setText("Hola mundo");
 
         //Actividades
-        //a) Modifica el texto "Hola mundo", pon tu nombre y ejecuta la aplicacion
-        //b) Crea una entrada en la carpeta res/values/strings.xml con tu numero de cuenta
-        //c) modifica setText para que lea esta entrada de texto (Tip, puedes hacer referencia al texto con R.string.<id entrada>)
-        //d) Ejecuta la aplicacion
+        //a) Agrega un evento  setOnClickListener al boton como viene en la referencia
+        //b) El el metodo onClick, modifica el texto de tv para que muestre la fecha actual con LocalDateTime.now()
         //e) Sube tu codigo al repositorio.
         //f) Sube un documento en word a la plataforma Moodle con las capturas de pantalla de tu actividad. Incluye la liga a tu repositorio
 
