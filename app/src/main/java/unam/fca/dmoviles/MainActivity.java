ca832761dca301054d20913b56aeff7cc22c72e1
rocio <rocio.barragan8@gmail.com>
2019-03-13 15:49:00 -0600

    Se hizo la actividad 2

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,8 +2,11 @@ package unam.fca.dmoviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.time.LocalDateTime;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +26,17 @@ public class MainActivity extends AppCompatActivity {
         //Para hacer referencia a un componente del layout, utilizamos el metodo findViewById
         //y le pasamos como parametro el id correspondiente.
 
-        final TextView tv = findViewById(R.id.btn);
+        final TextView tv = findViewById(R.id.tv);
 
         final Button btn = findViewById(R.id.btn);
 
-
-        //Para cambiar el texto de tv
-        tv.setText("Hola mundo");
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                // Para cambiar el texto de tv
+                tv.setText(LocalDateTime.now().toString());
+            }
+        });
 
         //Actividades
         //a) Agrega un evento  setOnClickListener al boton como viene en la referencia
