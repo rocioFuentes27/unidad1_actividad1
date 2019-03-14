d7bb724a6b3b05b421d74169d98b044adb28297a
rocio <rocio.barragan8@gmail.com>
2019-03-14 15:25:04 -0600

    Se realizo la actividad 3

--- app/src/main/java/unam/fca/dmoviles/SegundaActividad.java
+++ app/src/main/java/unam/fca/dmoviles/SegundaActividad.java
@@ -1,8 +1,7 @@
 package unam.fca.dmoviles;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,8 +14,11 @@ public class SegundaActividad extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_actividad);
 
+        // obtenemos el mensaje del mainActivity
+        Intent intent = getIntent();
+        String msj = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView tv = findViewById(R.id.tv);
-
+        tv.setText(msj); // cambiamos el texto de tv
     }
 
 }
