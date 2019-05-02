c6586843e49d7643209c3f52b22748d5b2ad87eb
rocio <rocio.barragan8@gmail.com>
2019-05-02 15:28:07 -0500

    Se realizo la actividad 8

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,10 +2,16 @@ package unam.fca.dmoviles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int IMAGE_WIDTH = 400;
+    private final static String TEXT_VIEW_KEY = "TEXT_VIEW_KEY";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,29 @@ public class MainActivity extends AppCompatActivity {
         // El archivo del layout de la actividad se encuentra en la carpeta
         // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // Hacemos referencia a un componente del layout
+        final EditText et1 = findViewById(R.id.editText);
+        final EditText et2 = findViewById(R.id.editText2);
+        final Button btn = findViewById(R.id.button);
+        final TextView resultado = findViewById(R.id.textViewResult);
+
+        // Llamamos al evento onclick
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                // Obtenemos los valores ingresados
+                int num1 = Integer.parseInt(et1.getText().toString());
+                int num2 = Integer.parseInt(et2.getText().toString());
+
+                // Realizamos la operacion
+                float absnum1 = Math.abs(num1);
+                float res = ((num2 - num1) / absnum1) * 100;
+
+                // Mostramos el resultado
+                resultado.setText(""+res+" %");
+            }
+        });
 
 
 
@@ -33,5 +62,28 @@ public class MainActivity extends AppCompatActivity {
         //g) Sube el documento Word a la plataforma Moodle. Incluye la liga a tu repositorio
 
 
+    }
+    @Override
+    // Funcion para restaurar el estado de una actividad despues de ser destruida
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        final EditText et1 = findViewById(R.id.editText);
+        //final EditText et2 = findViewById(R.id.editText2);
+        final TextView tv = findViewById(R.id.textViewResult);
+        et1.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+        //et2.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+        tv.setText(savedInstanceState.getString(TEXT_VIEW_KEY));
+    }
+
+
+    @Override
+    // Funcion para guardar el estado de una interfaz
+    public void onSaveInstanceState(Bundle outState) {
+        final EditText et1 = findViewById(R.id.editText);
+        final EditText et2 = findViewById(R.id.editText2);
+        final TextView tv = findViewById(R.id.textViewResult);
+        outState.putString(TEXT_VIEW_KEY, et1.getText().toString());
+        outState.putString(TEXT_VIEW_KEY, et2.getText().toString());
+        //outState.putString(TEXT_VIEW_KEY, tv.getText().toString());
+        super.onSaveInstanceState(outState);
     }
 }
